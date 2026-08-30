c2c6cf6b71ca5d3ac38de92de3065eb382a39e81
MaxHandMade <maxhandmadeonline@gmail.com>
2026-08-30 03:35:49 +0300

    test: update shipped surface count after LayoutEngine

--- tests/ShippedSurfaceTest.php
+++ tests/ShippedSurfaceTest.php
@@ -36,8 +36,8 @@ final class ShippedSurfaceTest extends TestCase {
 		$p1 = array_filter( $files, static fn( $p ) => (bool) preg_match( '/\.(css|js|jsx|php)$/', $p ) );
 		$p2 = array_filter( $files, static fn( $p ) => str_ends_with( $p, '.css' ) );
 
-		self::assertCount( 28, $files, 'shipped file count changed' );
-		self::assertCount( 21, $p1, 'P1 file set changed' );
+		self::assertCount( 29, $files, 'shipped file count changed' );
+		self::assertCount( 22, $p1, 'P1 file set changed' );
 		self::assertCount( 1, $p2, 'P2 file set changed' );
 	}
 }
